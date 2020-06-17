67eb4296290b3f8192e1ca1b09f1017e8c964503
krystian-rymon-lipinski <rymon9696@gmail.com>
2020-06-17 16:14:52 +0200

    Add tests for tree methods

--- src/test/java/com/krystianrymonlipinski/tree/model/TreeTest.java
+++ src/test/java/com/krystianrymonlipinski/tree/model/TreeTest.java
@@ -12,4 +12,67 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class TreeTest {
 
 	Tree<Integer, String> testObj;
+	
+	@Before
+	public void setUp() {
+		Tree<Integer, String> tree = new Tree(new Node<Integer, String>(Node.Type.ROOT_NODE));
+		testObj = Mockito.spy(tree);
+	}
+
+	@Test
+	public void createTree() {
+		Node<Integer> root = new Node<Integer, String>(Node.Type.ROOT_NODE);
+		Tree<Integer, String> tree = new Tree(root);
+		assertEquals(1, tree.getNodes().getSize());
+		assertEquals(root, tree.getRoot());
+		assertEquals(root, tree.getCurrentNode());
+	}
+
+	@Test
+	public void addNode_secondRootError() {
+		Node<Integer, String> newNode = new Node<Integer, String>(Node.Type.ROOT_NODE);
+
+	}
+
+	@Test
+	public void addNode_correctCreation() {
+		Node<Integer> newNode = testObj.addNode(testObj.getRoot(), "first");
+		assertEquals(testObj.getRoot(), testObj.getCurrentNode());
+		assertEquals(2, testObj.getNodes().size());
+		assertEquals(1, testObj.getRoot().getChildren().size());
+		assertEquals(newNode, testObj.getRoot().getChildren().get(0));
+	}
+
+	@Test
+	public void moveDown() {
+		Node<Integer> newNode = testObj.addNode(testObj.getRoot(), "node");
+		testObj.moveDown("node");
+		assertEquals(newNode, testObj.getCurrentNode());
+	}
+
+	@Test
+	public void moveUp() {
+		Node<Integer> newNode = testObj.addNode(testObj.getRoot(), "node");
+		testObj.moveDown("node");
+		testObj.moveUp();
+		assertEquals(testObj.getRoot(), testObj.getCurrentNode());
+	}
+
+	@Test 
+	public void removeNode_withoutChildren() {
+		Node<Integer> newNode = testObj.addNode(testObj.getRoot(), "node");
+		testObj.removeNode(newNode);
+		assertEquals(1, testObj.getNodes().size());
+	}
+
+	@Test
+	public void removeNode_withChilden() {
+		Node<Integer> newNode = testObj.addNode(testObj.getRoot(), "first_level");
+		testObj.addNode(newNode, "second_level_one");
+		testObj.addNode(newNode, "second_level_two");
+		testObj.removeNode(newNode);
+
+		assertEquals(1, testObj.getNodes().getSize());
+	}
+	
 }
